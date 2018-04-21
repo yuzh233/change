df217062d63178bbea2f16e8dcbebd27443ebd34
cchangee <web_cheng@163.com>
2018-04-21 18:24:57 +0800

    add BaseControllerImpl

--- change-common/change-common-base/src/main/java/me/cathub/change/common/base/BaseControllerImpl.java
+++ change-common/change-common-base/src/main/java/me/cathub/change/common/base/BaseControllerImpl.java
@@ -10,68 +10,68 @@ import java.util.List;
 public abstract class BaseControllerImpl<B extends Serializable, S extends BaseRpcServer> implements BaseCrud<B> {
 
     @Autowired
-    S service;
+    S rpcService;
 
     @RequestMapping("/insert")
     @ResponseBody
     @Override
     public boolean insert(B bean) throws Exception {
-        return service.insert(bean);
+        return rpcService.insert(bean);
     }
 
     @RequestMapping("/deleteL")
     @ResponseBody
     @Override
     public boolean deleteL(B bean) throws Exception {
-        return service.deleteL(bean);
+        return rpcService.deleteL(bean);
     }
 
     @RequestMapping("/restore")
     @ResponseBody
     @Override
     public boolean restore(B bean) throws Exception {
-        return service.restore(bean);
+        return rpcService.restore(bean);
     }
 
     @RequestMapping("/deleteP")
     @ResponseBody
     @Override
     public boolean deleteP(B bean) throws Exception {
-        return service.deleteP(bean);
+        return rpcService.deleteP(bean);
     }
 
     @RequestMapping("/update")
     @ResponseBody
     @Override
     public boolean update(B bean) throws Exception {
-        return service.update(bean);
+        return rpcService.update(bean);
     }
 
     @RequestMapping("/select")
     @ResponseBody
     @Override
     public B select(B bean) throws Exception {
-        return (B) service.select(bean);
+        return (B) rpcService.select(bean);
     }
 
     @RequestMapping("/list")
     @ResponseBody
     @Override
     public List<B> list(int page, int count, int tableIndex) throws Exception {
-        return service.list(page, count, tableIndex);
+        return rpcService.list(page, count, tableIndex);
     }
 
     @RequestMapping("/count")
     @ResponseBody
     @Override
     public long count(int tableIndex) throws Exception {
-        return service.count(tableIndex);
+        return rpcService.count(tableIndex);
     }
 
     @RequestMapping("/clear")
     @ResponseBody
     @Override
     public long clear(int tableIndex) throws Exception {
-        return service.clear(tableIndex);
+        return rpcService.clear(tableIndex);
     }
 }
